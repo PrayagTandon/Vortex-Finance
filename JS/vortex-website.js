865e194d839be7bc6454a68eed344746583ac831
Prayag Tandon <tandonprayag99@gmail.com>
2024-01-25 13:02:28 +0530

    ...

--- JS/vortex-website.js
+++ JS/vortex-website.js
@@ -2,5 +2,19 @@
 
 /* SELECTING ELEMENTS */
 
+// ELEMENTS
 const header = document.querySelector('.header');
 const navbar = document.querySelector('.header__nav');
+const linkHeader = document.querySelector('.header__nav--link-item');
+const logo = document.querySelector('.header__nav--logo-link');
+
+// SECTION
+const sectionFeatures = document.querySelector('section--features');
+const sectionServices = document.querySelector('.section--services');
+const sectionTab = document.querySelector('.section--tab');
+const sectionTestimonials = document.querySelector('.section--testimonials');
+const footer = document.querySelector('.footer');
+
+// BUTTONS
+const btnFill = document.querySelector('.btn--fill');
+const btnOutline = document.querySelector('.btn--outline');
